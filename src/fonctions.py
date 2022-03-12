915fe48e1ab6b3ff671eadeb9453e4b1c0b5d74e
ColineVL <coline.vanleeuwen@gmail.com>
2022-03-12 17:37:10 +0100

    mauvaise conversion

--- src/fonctions.py
+++ src/fonctions.py
@@ -10,9 +10,15 @@ def conversionDegreToRadian(angleDegre):
     return angleDegre * pi / 180
 
 
+assert conversionDegreToRadian(180) == pi
+
+
 def conversionKmhToMs(kmh):
     """Convertit une vitesse en km/h en m/s"""
     return kmh / 3.6
+
+
+assert conversionKmhToMs(3.6) == 1
 
 
 def conversionKmhToKms(kmh):
@@ -20,9 +26,22 @@ def conversionKmhToKms(kmh):
     return kmh / 3600
 
 
+assert conversionKmhToKms(3.6) == 0.001
+
+
 def conversionTourParMinuteToRadParSeconde(tourParMinute):
     """Convertit une vitesse de rotation de tours/minute en rad/s"""
     return tourParMinute * (2 * pi / 60)
+
+
+assert conversionTourParMinuteToRadParSeconde(30) == pi
+
+
+def conversionM3ToLitres(m3):
+    return m3 * 1000
+
+
+assert conversionM3ToLitres(1) == 1000
 
 
 def conversionCarburantKgToLitres(kg):
@@ -30,17 +49,22 @@ def conversionCarburantKgToLitres(kg):
     # Conversion en m3
     m3 = kg / rho_carb
     # Conversion en litres
-    litres = m3 * 1000
-    return litres
+    return conversionM3ToLitres(m3)
 
 
 def conversionMJTokWh(MJ):
     """Convertit énergie de MJ à kWh"""
-    return MJ / (3.6 * 10**12)
+    return MJ / 3.6
+
+
+assert conversionMJTokWh(3.6) == 1
 
 
 def conversionSecondeToHeure(seconde):
     return seconde / 3600
+
+
+assert conversionSecondeToHeure(3600) == 1
 
 
 def plot(data, titre):
